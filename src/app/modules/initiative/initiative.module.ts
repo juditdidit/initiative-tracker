da24e702acba954a3d8797c2d9ee0b69240e0fc9
Judit <hello@juditdidit.com>
2020-12-26 09:16:15 -0800

    why don't you wokr

--- src/app/modules/initiative/initiative.module.ts
+++ src/app/modules/initiative/initiative.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 import { InitiativeRoutingModule } from './initiative-routing.module';
 import { AddCreatureComponent } from './add-creature/add-creature.component';
